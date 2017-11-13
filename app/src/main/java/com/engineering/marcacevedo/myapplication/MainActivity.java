f1c904ba8548a4da32e58764ea4af8ba8fa0eefb
Marc Acevedo <mcacevedo@loyola.edu>
2017-11-13 17:50:07 -0500

    playing

--- app/src/main/java/com/engineering/marcacevedo/myapplication/MainActivity.java
+++ app/src/main/java/com/engineering/marcacevedo/myapplication/MainActivity.java
@@ -2,12 +2,45 @@ package com.engineering.marcacevedo.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewGroup mContentView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().setBackgroundDrawable();
+
+        //mContentView = (ViewGroup) findViewById(R.id.activity_main);
+
+        mContentView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                setToFullScreen();
+            }
+        });
+    }
+
+    private void setToFullScreen()
+    {
+        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setToFullScreen();
     }
 }
