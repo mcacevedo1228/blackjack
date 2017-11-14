ac3cde66be8bc8fd470089866ec34e3941d1f145
Marc Acevedo <mcacevedo@loyola.edu>
2017-11-13 19:34:14 -0500

    Game Logic progress

--- app/src/main/java/com/engineering/marcacevedo/myapplication/MainActivity.java
+++ app/src/main/java/com/engineering/marcacevedo/myapplication/MainActivity.java
@@ -1,46 +1,51 @@
 package com.engineering.marcacevedo.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ViewGroup mContentView;
+    private Button btn1 = (Button)findViewById(R.id.New_Game_Button);
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getWindow().setBackgroundDrawable();
+        setActivityBackgroundColor();
+    }
 
-        //mContentView = (ViewGroup) findViewById(R.id.activity_main);
-
-        mContentView.setOnClickListener(new View.OnClickListener()
+    btn1.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v)
         {
-            @Override
-            public void onClick(View view)
-            {
-                setToFullScreen();
-            }
-        });
-    }
-
-    private void setToFullScreen()
-    {
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-    }
+            startActivity(new Intent(MainActivity.this, newActivity.class));
+        }
+    });
 
     @Override
     protected void onResume() {
         super.onResume();
-        setToFullScreen();
     }
+
+    public void setActivityBackgroundColor() {
+        View view = this.getWindow().getDecorView();
+        view.setBackgroundColor(getResources().getColor(android.R.color.holo_green_light));
+    }
+
+
+
+
+
+
+
+
 }
---
+++ app/src/main/java/models/Deck.java
@@ -0,0 +1,8 @@
+package models;
+
+/**
+ * Created by marcacevedo on 11/13/17.
+ */
+
+public class Deck {
+}
